b2ac461ec72053a2248c21e59f9b6a39a8cb89d4
Sergey Kritskiy <krickiy_sp@bw-sw.com>
2016-08-16 16:17:59 +0700

    Linux settings added

--- src/CheckListServer/Startup.cs
+++ src/CheckListServer/Startup.cs
@@ -1,114 +1,111 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
+﻿#define LINUX
+
+using System;
 using System.Threading.Tasks;
 using DAL.Contexts;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 
 namespace CheckListServer
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IHostingEnvironment env)
     {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+      var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
-            if (env.IsEnvironment("Development"))
-            {
-                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
-                builder.AddApplicationInsightsSettings(developerMode: true);
-            }
+      if (env.IsEnvironment("Development"))
+      {
+        builder.AddApplicationInsightsSettings(developerMode: true);
+      }
 
-            builder.AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
-
-        public IConfigurationRoot Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            services.AddAuthorization();
-            services.AddApplicationInsightsTelemetry(Configuration);
-            var connection = @"Server=(localdb)\mssqllocaldb;Database=Tasks;Trusted_Connection=True;";
-            services.AddDbContext<ProjectContext>(options =>
-            {
-                options.UseSqlServer(connection, b=> b.MigrationsAssembly("CheckListServer"));
-            });
-
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            app.UseCookieAuthentication(new CookieAuthenticationOptions()
-            {
-                AuthenticationScheme = "Cookie",
-                LoginPath = new PathString("/Account/Login/"),
-                AccessDeniedPath = new PathString("/Account/Forbidden/"),
-                AutomaticAuthenticate = true,
-                AutomaticChallenge = true
-            });
-
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-            
-            app.UseApplicationInsightsRequestTelemetry();
-
-            app.UseApplicationInsightsExceptionTelemetry();
-
-            app.UseMvc();
-
-           
-        }
+      builder.AddEnvironmentVariables();
+      Configuration = builder.Build();
     }
 
-    public class MyTicketStore : ITicketStore
+    public IConfigurationRoot Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
     {
-        public Task StoreAsync(AuthenticationTicket ticket)
-        {
-            throw new NotImplementedException();
-        }
+      services.AddAuthorization();
+      services.AddApplicationInsightsTelemetry(Configuration);
+#if LINUX
+      var connection = "Server=172.17.0.1;User Id=postgres;Port=5432;Database=Tasks;";
+      services.AddDbContext<ProjectContext>(
+        options => { options.UseNpgsql(connection, b => b.MigrationsAssembly("CheckListServer")); });
+#else
+          var connection = @"Server=(localdb)\mssqllocaldb;Database=Tasks;";
+          services.AddDbContext<ProjectContext>(
+              options => { options.UseSqlServer(connection, b => b.MigrationsAssembly("CheckListServer")); });
+#endif
 
-        public Task RemoveAsync(string key)
-        {
-            throw new NotImplementedException();
-        }
 
-        public Task RetrieveAsync(string key)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task RenewAsync(string key, AuthenticationTicket ticket)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<string> ITicketStore.StoreAsync(AuthenticationTicket ticket)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<AuthenticationTicket> ITicketStore.RetrieveAsync(string key)
-        {
-            throw new NotImplementedException();
-        }
+      services.AddMvc();
     }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      app.UseCookieAuthentication(new CookieAuthenticationOptions()
+      {
+        AuthenticationScheme = "Cookie",
+        LoginPath = new PathString("/Account/Login/"),
+        AccessDeniedPath = new PathString("/Account/Forbidden/"),
+        AutomaticAuthenticate = true,
+        AutomaticChallenge = true
+      });
+
+      loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+      loggerFactory.AddDebug();
+
+      app.UseApplicationInsightsRequestTelemetry();
+
+      app.UseApplicationInsightsExceptionTelemetry();
+
+      app.UseMvc();
+    }
+  }
+
+  public class MyTicketStore : ITicketStore
+  {
+    public Task StoreAsync(AuthenticationTicket ticket)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task RemoveAsync(string key)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task RetrieveAsync(string key)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task RenewAsync(string key, AuthenticationTicket ticket)
+    {
+      throw new NotImplementedException();
+    }
+
+    Task<string> ITicketStore.StoreAsync(AuthenticationTicket ticket)
+    {
+      throw new NotImplementedException();
+    }
+
+    Task<AuthenticationTicket> ITicketStore.RetrieveAsync(string key)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
